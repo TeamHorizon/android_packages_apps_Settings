2e10d690b24c4af773a283e4a061548b5abd571c
iceandfire <arhamjamal@gmail.com>
2013-03-25 23:49:18 +0530

    XenonHD logo in about display accurate XenonHD version updated with xehdversion
    Thanks to dax
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -86,7 +86,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
-        setValueSummary(KEY_XEHD_VERSION, "ro.xehdversion");
+        setValueSummary(KEY_XEHD_VERSION, "ro.modversion");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
