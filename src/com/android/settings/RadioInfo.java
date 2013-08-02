f24546de51bfb5547db0dcecd78a463c47ac4738
Sungmin Choi <sungmin.choi@lge.com>
2013-08-02 14:33:58 -0700

    Add LTE/WCDMA to network preferred mode
    Change-Id: I1cab3df9a515f7bf51c861587c52771cc7897f18

--- src/com/android/settings/RadioInfo.java
+++ src/com/android/settings/RadioInfo.java
@@ -1090,6 +1090,7 @@ public class RadioInfo extends Activity {
             "LTE/GSM auto (PRL)",
             "LTE/GSM/CDMA auto (PRL)",
             "LTE only",
+            "LTE/WCDMA",
             "Unknown"};
 
     private void log(String s) {
