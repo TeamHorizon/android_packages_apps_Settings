478075583916a3a96e1948a9e8adbb0a962e3353
Lokesh Chamane <lokesh.c703@gmail.com>
2015-02-26 21:56:29 -0700

    Settings: use switch preference for Ambient Display
    Change-Id: I2dc12b83ac24ac7a120d2a8808c17b2425c118ef
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	src/com/android/settings/xenonhd/AmbientSettings.java

--- src/com/android/settings/search/SearchIndexableResources.java
+++ src/com/android/settings/search/SearchIndexableResources.java
@@ -46,12 +46,13 @@ import com.android.settings.notification.OtherSoundSettings;
 import com.android.settings.notification.ZenModeSettings;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.sim.SimSettings;
-import com.android.settings.temasek.AmbientSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.voice.VoiceInputSettings;
 import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
+import com.android.settings.xenonhd.AmbientSettings;
+
 
 import java.util.Collection;
 import java.util.HashMap;
