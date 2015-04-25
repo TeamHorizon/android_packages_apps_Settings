34ccb6108be33be9654cd1f8d4bf5e117f514366
iceandfire <arhamjamal@gmail.com>
2015-04-25 20:49:50 +0530

    Fix mobile networks dashboard category appearing on devices without telephony support
    Change-Id: I281e4aa68bd1659ef99fe71adb777047e0962918
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1186,6 +1186,13 @@ public class SettingsActivity extends Activity
                     if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
                         removeTile = true;
                     }
+                } else if (id == R.id.mobile_networks) {
+                    // Remove Mobile Networks Settings if Telephony service is not available.
+                    if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+                        removeTile = true;
+                    } else if (TelephonyManager.getDefault().getPhoneCount() > 1) {
+                        removeTile = true;
+                    }
                 } else if (id == R.id.data_usage_settings) {
                     // Remove data usage when kernel module not enabled
                     final INetworkManagementService netManager = INetworkManagementService.Stub
