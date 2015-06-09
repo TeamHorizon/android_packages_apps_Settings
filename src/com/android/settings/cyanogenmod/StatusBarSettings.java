e4107213a39adad5a5100c58d0083afa6c453167
iceandfire <arhamjamal@gmail.com>
2015-06-09 18:26:52 +0530

    Fix missing import
    Change-Id: I4f7033d8476094e24a9072f3c67a0bbc6b8d06ca
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- src/com/android/settings/cyanogenmod/StatusBarSettings.java
+++ src/com/android/settings/cyanogenmod/StatusBarSettings.java
@@ -23,6 +23,7 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
