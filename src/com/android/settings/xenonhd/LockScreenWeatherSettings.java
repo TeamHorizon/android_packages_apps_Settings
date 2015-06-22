siracuervo/android_packages_apps_Settings-4

Lock screen: Weather panel, (WIP), (2/2)

src/com/android/settings/darkkat/LockScreenWeatherSettings.java
@@ -1,0 +1,97 @@
+/* 
+ * Copyright (C) 2014 DarkKat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.darkkat;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockScreenWeatherSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_SHOW_WEATHER =
+            "weather_show_weather";
+    private static final String PREF_SHOW_LOCATION =
+            "weather_show_location";
+
+    private CheckBoxPreference mShowWeather;
+    private CheckBoxPreference mShowLocation;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        addPreferencesFromResource(R.xml.lock_screen_weather_settings);
+        mResolver = getActivity().getContentResolver();
+
+        boolean showWeather = Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_SHOW_WEATHER, 0) == 1;
+
+        mShowWeather =
+                (CheckBoxPreference) findPreference(PREF_SHOW_WEATHER);
+        mShowWeather.setChecked(showWeather);
+        mShowWeather.setOnPreferenceChangeListener(this);
+
+        if (showWeather) {
+            mShowLocation =
+                    (CheckBoxPreference) findPreference(PREF_SHOW_LOCATION);
+            mShowLocation.setChecked(Settings.System.getInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION, 1) == 1);
+            mShowLocation.setOnPreferenceChangeListener(this);
+        } else {
+            removePreference(PREF_SHOW_LOCATION);
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value;
+        String hex;
+        int intHex;
+
+        if (preference == mShowWeather) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                Settings.System.LOCK_SCREEN_SHOW_WEATHER,
+                value ? 1 : 0);
+            refreshSettings();
+            return true;
+        } else if (preference == mShowLocation) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION,
+                value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+}

TeamHorizon/android_packages_apps_Settings

Signed-off-by: iceandfire <arhamjamal@gmail.com>
Conflicts:
	AndroidManifest.xml
	res/values-de/dk_strings.xml
	res/values/dk_strings.xml
	res/xml/dashboard_categories.xml
	src/com/android/settings/SettingsActivity.java
Change-Id: I04d833e1ae18fe54e45a49b2f2afd77c39de90df

src/com/android/settings/xenonhd/LockScreenWeatherSettings.java
@@ -1,0 +1,97 @@
+/* 
+ * Copyright (C) 2014 DarkKat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.xenonhd;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockScreenWeatherSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_SHOW_WEATHER =
+            "weather_show_weather";
+    private static final String PREF_SHOW_LOCATION =
+            "weather_show_location";
+
+    private CheckBoxPreference mShowWeather;
+    private CheckBoxPreference mShowLocation;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        addPreferencesFromResource(R.xml.lock_screen_weather_settings);
+        mResolver = getActivity().getContentResolver();
+
+        boolean showWeather = Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_SHOW_WEATHER, 0) == 1;
+
+        mShowWeather =
+                (CheckBoxPreference) findPreference(PREF_SHOW_WEATHER);
+        mShowWeather.setChecked(showWeather);
+        mShowWeather.setOnPreferenceChangeListener(this);
+
+        if (showWeather) {
+            mShowLocation =
+                    (CheckBoxPreference) findPreference(PREF_SHOW_LOCATION);
+            mShowLocation.setChecked(Settings.System.getInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION, 1) == 1);
+            mShowLocation.setOnPreferenceChangeListener(this);
+        } else {
+            removePreference(PREF_SHOW_LOCATION);
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value;
+        String hex;
+        int intHex;
+
+        if (preference == mShowWeather) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                Settings.System.LOCK_SCREEN_SHOW_WEATHER,
+                value ? 1 : 0);
+            refreshSettings();
+            return true;
+        } else if (preference == mShowLocation) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION,
+                value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+}