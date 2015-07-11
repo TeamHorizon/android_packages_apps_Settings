ed383f7d2cada0a4c95b0ff2a026875912a17549
Peter Carenza <pcarenza@gmail.com>
2015-07-11 15:06:19 -0400

    About XenonHD
    Colorful way of showing off our team
    
    preference fragment: DeveloperPreference.java
    preference screen: prefs_about.xml
    layout: dev_card.xml
    
    Change-Id: I51cec3f6f3375474c3d7e166a465098254aa91e5

---
+++ src/com/android/settings/About.java
@@ -0,0 +1,74 @@
+package com.android.settings;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class About extends SettingsPreferenceFragment {
+
+
+    public static final String TAG = "About";
+
+    Preference mSiteUrl;
+    Preference mSourceUrl;
+    Preference mGerritUrl;
+    Preference mSponsorUrl;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.title_about);
+        addPreferencesFromResource(R.xml.prefs_about);
+        mSiteUrl = findPreference("xenonhd_website");
+        mSourceUrl = findPreference("xenonhd_source");
+	mGerritUrl = findPreference("xenonhd_gerrit");
+	mSponsorUrl = findPreference("xenonhd_picture");
+
+//        PreferenceGroup devsGroup = (PreferenceGroup) findPreference("devs");
+//        int mSize = devsGroup.getPreferenceCount();
+//	Log.i(TAG,"There are a total of "+mSize+" preferences.");
+/*        ArrayList<Preference> devs = new ArrayList<Preference>();
+        devs.clear();
+        for (int i = 0; i < mSize; i++) {
+            devs.add(devsGroup.getPreference(i));
+            Log.i(TAG,"Added preference "+i+" of "+mSize);
+        }
+        devsGroup.removeAll();
+        devsGroup.setOrderingAsAdded(false);
+        for(int i = 0; i < mSize; i++) {
+            Preference p = devs.get(i);
+            p.setOrder(i);
+
+            devsGroup.addPreference(p);
+        }
+*/
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+	if (preference == mSiteUrl) {
+            launchUrl("http://xenonhd.com");
+        } else if (preference == mSourceUrl) {
+            launchUrl("http://github.com/TeamHorizon");
+        } else if (preference == mGerritUrl) {
+	    launchUrl("http://83.233.5.249:8081");
+        } else if (preference == mSponsorUrl) {
+            launchUrl("https://www.kazi-networks.com/link.php?id=15");
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void launchUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent website = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(website);
+    }
+}
