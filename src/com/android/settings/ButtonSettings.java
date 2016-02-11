5ab489063e9dc6dc880175b2b3b0d5d7da9b1b93
ljjehl <ljjehl@gmail.com>
2016-02-11 16:19:47 +0100

    Update HW rebindings
    Change-Id: I1a74afe3b3ba19c71889f3ab7c9611b71bd2fa14

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -1,45 +1,35 @@
 /*
-* Copyright (C) 2015 Team Horizon
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2013 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.settings;
 
-import android.os.Bundle;
-
-import com.android.internal.logging.MetricsLogger;
-
-import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.Handler;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
-import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
-import android.provider.SearchIndexableResource;
-import android.preference.CheckBoxPreference;
 import android.provider.Settings;
 
 import android.util.Log;
@@ -47,55 +37,11 @@ import android.view.IWindowManager;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.WindowManagerGlobal;
+
 import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
-
-public class ButtonSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
-
-	private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
-	private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
-	private static final String KEY_VOLUME_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
-
-	private ListPreference mVolumeKeyCursorControl;
-	private SwitchPreference mVolumeRockerWake;
-	private SwitchPreference mVolumeControlRingStream;
-
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.button_settings);
-
-		// volume key cursor control
-        mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
-        if (mVolumeKeyCursorControl != null) {
-            mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
-            int volumeRockerCursorControl = Settings.System.getInt(getContentResolver(),
-                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
-            mVolumeKeyCursorControl.setValue(Integer.toString(volumeRockerCursorControl));
-            mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
-        }
-
-		mVolumeRockerWake = (SwitchPreference) findPreference(VOLUME_ROCKER_WAKE);
-        mVolumeRockerWake.setOnPreferenceChangeListener(this);
-        int volumeRockerWake = Settings.System.getInt(getContentResolver(),
-                VOLUME_ROCKER_WAKE, 0);
-        mVolumeRockerWake.setChecked(volumeRockerWake != 0);
-
-		mVolumeControlRingStream = (SwitchPreference)
-                findPreference(KEY_VOLUME_CONTROL_RING_STREAM);
-        int volumeControlRingtone = Settings.System.getInt(getContentResolver(),
-                Settings.System.VOLUME_KEYS_CONTROL_RING_STREAM, 1);
-        if (mVolumeControlRingStream != null) {
-            mVolumeControlRingStream.setChecked(volumeControlRingtone > 0);
-        }
-    }
-
-	private void handleActionListChange(ListPreference pref, Object newValue, String setting) {
-        String value = (String) newValue;
-        int index = pref.findIndexOfValue(value);
 
 public class ButtonSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -235,16 +181,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         Settings.System.putInt(getContentResolver(), setting, Integer.valueOf(value));
     }
 
-	@Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-		if (preference == mVolumeKeyCursorControl) {
-            handleActionListChange(mVolumeKeyCursorControl, newValue,
-                    Settings.System.VOLUME_KEY_CURSOR_CONTROL);
-            return true;
-		} else if (preference == mVolumeRockerWake) {
-            Settings.System.putInt(getActivity().getContentResolver(),
-                Settings.System.VOLUME_ROCKER_WAKE,
-                    ((Boolean) newValue) ? 1 : 0);
     @Override
     protected int getMetricsCategory() {
         return MetricsLogger.DONT_TRACK_ME_BRO;
@@ -270,17 +206,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             return true;
         }
         return false;
-    }
-
-	@Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mVolumeControlRingStream) {
-            int value = mVolumeControlRingStream.isChecked() ? 1 : 0;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.VOLUME_KEYS_CONTROL_RING_STREAM, value);
-        }
-
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
 }
