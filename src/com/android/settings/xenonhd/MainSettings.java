32dd375ad4cb5df4d02d485dcfa9a37f9d7071d9
XXMrHyde <xxmrhyde@gmx.co.uk>
2017-01-11 19:57:22 +0100

    Custom QS rows/columns [2/2]
    *DU Edits*
    - Cleaned up some of the redundancy
    
    ** Xenonify by dadi11
    
    Change-Id: I7906781ab30ad977c4de0db315c8c06bf42bf6ef

--- src/com/android/settings/xenonhd/MainSettings.java
+++ src/com/android/settings/xenonhd/MainSettings.java
@@ -53,10 +53,20 @@ public class MainSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "MainSettings";
+
     private static final String SELINUX = "selinux";
 
+    private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
+    private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
+    private static final String PREF_COLUMNS = "qs_columns";
+
     private SwitchPreference mConfig;
+
     private SwitchPreference mSelinux;
+
+    private ListPreference mRowsPortrait;
+    private ListPreference mRowsLandscape;
+    private ListPreference mQsColumns;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -79,6 +89,38 @@ public class MainSettings extends SettingsPreferenceFragment implements
     }
 
     @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        int defaultValue;
+
+        mRowsPortrait = (ListPreference) findPreference(PREF_ROWS_PORTRAIT);
+        int rowsPortrait = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.QS_ROWS_PORTRAIT, 3);
+        mRowsPortrait.setValue(String.valueOf(rowsPortrait));
+        mRowsPortrait.setSummary(mRowsPortrait.getEntry());
+        mRowsPortrait.setOnPreferenceChangeListener(this);
+
+        defaultValue = getResources().getInteger(com.android.internal.R.integer.config_qs_num_rows_landscape_default);
+        mRowsLandscape = (ListPreference) findPreference(PREF_ROWS_LANDSCAPE);
+        int rowsLandscape = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.QS_ROWS_LANDSCAPE, defaultValue);
+        mRowsLandscape.setValue(String.valueOf(rowsLandscape));
+        mRowsLandscape.setSummary(mRowsLandscape.getEntry());
+        mRowsLandscape.setOnPreferenceChangeListener(this);
+
+        mQsColumns = (ListPreference) findPreference(PREF_COLUMNS);
+        int columnsQs = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.QS_COLUMNS, 3);
+        mQsColumns.setValue(String.valueOf(columnsQs));
+        mQsColumns.setSummary(mQsColumns.getEntry());
+        mQsColumns.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
     protected int getMetricsCategory() {
         return MetricsEvent.APPLICATION;
     }
@@ -95,18 +137,43 @@ public class MainSettings extends SettingsPreferenceFragment implements
      }
 
      @Override
-     public boolean onPreferenceChange(Preference preference, Object value) {
+     public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+        int intValue;
+        int index;
+
      ContentResolver resolver = getActivity().getContentResolver();
             if (preference == mSelinux) {
-            if (value.toString().equals("true")) {
+            if (objValue.toString().equals("true")) {
                 CMDProcessor.runSuCommand("setenforce 1");
                 mSelinux.setSummary(R.string.selinux_enforcing_title);
-            } else if (value.toString().equals("false")) {
+            } else if (objValue.toString().equals("false")) {
                 CMDProcessor.runSuCommand("setenforce 0");
                 mSelinux.setSummary(R.string.selinux_permissive_title);
             }
             return true;
-         }
+        } else if (preference == mRowsPortrait) {
+            intValue = Integer.valueOf((String) objValue);
+            index = mRowsPortrait.findIndexOfValue((String) objValue);
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.QS_ROWS_PORTRAIT, intValue);
+            preference.setSummary(mRowsPortrait.getEntries()[index]);
+            return true;
+        } else if (preference == mRowsLandscape) {
+            intValue = Integer.valueOf((String) objValue);
+            index = mRowsLandscape.findIndexOfValue((String) objValue);
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.QS_ROWS_LANDSCAPE, intValue);
+            preference.setSummary(mRowsLandscape.getEntries()[index]);
+            return true;
+        } else if (preference == mQsColumns) {
+            intValue = Integer.valueOf((String) objValue);
+            index = mQsColumns.findIndexOfValue((String) objValue);
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.QS_COLUMNS, intValue);
+            preference.setSummary(mQsColumns.getEntries()[index]);
+            return true;
+        }
         return false;
      } 
 }
