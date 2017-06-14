3bde226959bf459836372809919dcf911d1d6831
PMS22 <prathams99@rediff.com>
2017-06-14 07:10:15 +0000

    Correctly align cursor in Settings
    Steps to Reproduce:
    - Click on Density Settings (DPI Settings) / Renaming BT Device / Renaming SIM Settings
    - EditText Dialog pops up
    - The cursor is at the leftmost side of the text
    - Logically cursor should be at rightmost side which can be aligned grabbing text length
    
    Screenshots:
    Before:
    http://imgur.com/a/o4vkr
    http://imgur.com/a/6Qext
    
    After:
    http://imgur.com/a/fuxqT
    http://imgur.com/a/vm2WX
    
    Bug: 65185652
    Test: Manual test
    
    Change-Id: Id00308e7e019434edad4afab56cf4719be0d0d54
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- src/com/android/settings/bluetooth/BluetoothNameDialogFragment.java
+++ src/com/android/settings/bluetooth/BluetoothNameDialogFragment.java
@@ -141,7 +141,8 @@ public final class BluetoothNameDialogFragment extends InstrumentedDialogFragmen
         });
         mDeviceNameView.setText(deviceName);    // set initial value before adding listener
         mDeviceNameView.addTextChangedListener(this);
-        com.android.settings.Utils.setEditTextCursorPosition(mDeviceNameView);
+        int textLength = mDeviceNameView.getText().length();
+        mDeviceNameView.setSelection(textLength);
         mDeviceNameView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
