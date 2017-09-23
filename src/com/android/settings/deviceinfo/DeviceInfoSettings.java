be46839155106ca6f00bfef306afc66f00c1609d
dadi11 <dadi.sikorski@gmail.com>
2017-09-23 14:50:46 +0200

    Settings: remove XenonOTA link if not installed
    Change-Id: I2d15683e746204c9df572712bd8006cf37e4605c
    Signed-off-by: dadi11 <dadi.sikorski@gmail.com>

--- src/com/android/settings/deviceinfo/DeviceInfoSettings.java
+++ src/com/android/settings/deviceinfo/DeviceInfoSettings.java
@@ -35,6 +35,7 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.xenonhd.deviceinfo.XenonHDMaintainerPreferenceController;
 import com.android.settings.xenonhd.deviceinfo.XenonHDVersionPreferenceController;
+import com.android.settings.xenonhd.deviceinfo.XenonOTAPreferenceController;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 
@@ -143,6 +144,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
                 new BuildNumberPreferenceController(context, activity, fragment, lifecycle));
         controllers.add(new XenonHDMaintainerPreferenceController(context, lifecycle));
         controllers.add(new XenonHDVersionPreferenceController(context, lifecycle));
+        controllers.add(new XenonOTAPreferenceController(context));
         return controllers;
     }
 
